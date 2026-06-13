85905a31da08a4531193e38a9c5ffff0f9c67fd2
Leotens <leotens11@gmail.com>
2026-06-13 17:32:23 -0500

    feat: Add community infrastructure

---
+++ src/main/java/pe/greenminds/ecomind_backend/community/infrastructure/persistence/jpa/adapters/CommunityRepositoryImpl.java
@@ -0,0 +1,45 @@
+package pe.greenminds.ecomind_backend.community.infrastructure.persistence.jpa.adapters;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Repository;
+import pe.greenminds.ecomind_backend.community.domain.model.aggregates.Community;
+import pe.greenminds.ecomind_backend.community.domain.repositories.CommunityRepository;
+import pe.greenminds.ecomind_backend.community.infrastructure.persistence.jpa.assemblers.CommunityPersistenceAssembler;
+import pe.greenminds.ecomind_backend.community.infrastructure.persistence.jpa.repositories.CommunityPersistenceRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CommunityRepositoryImpl implements CommunityRepository {
+
+    private final CommunityPersistenceRepository communityPersistenceRepository;
+
+    public CommunityRepositoryImpl(CommunityPersistenceRepository communityPersistenceRepository) {
+        this.communityPersistenceRepository = communityPersistenceRepository;
+    }
+    @Override
+    public Optional<Community> findById(Long id) {
+        return communityPersistenceRepository.findById(id).map(CommunityPersistenceAssembler::toDomainFromPersistence);
+    }
+
+    @Override
+    public List<Community> search(String name, String location){
+        var nameFilter = name == null ? "" : name;
+        var locationFilter = location == null ? "" : location;
+
+        return communityPersistenceRepository
+                .search(nameFilter, locationFilter)
+                .stream()
+                .map(CommunityPersistenceAssembler::toDomainFromPersistence)
+                .toList();
+    }
+
+    @Override
+    public Community save(Community community) {
+        var savedEntity = communityPersistenceRepository.save(
+                CommunityPersistenceAssembler.toPersistenceFromDomain(community)
+        );
+        return CommunityPersistenceAssembler.toDomainFromPersistence(savedEntity);
+    }
+}
