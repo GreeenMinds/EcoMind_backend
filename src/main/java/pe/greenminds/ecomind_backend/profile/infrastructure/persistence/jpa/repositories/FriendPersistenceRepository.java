31ccbbece67f1ffd5e8bc2cdd5499a6678835e2a
kamil-tron <kamildiaz513@gmail.com>
2026-07-04 02:16:02 -0500

    feat: add notifications v2

--- src/main/java/pe/greenminds/ecomind_backend/profile/infrastructure/persistence/jpa/repositories/FriendPersistenceRepository.java
+++ src/main/java/pe/greenminds/ecomind_backend/profile/infrastructure/persistence/jpa/repositories/FriendPersistenceRepository.java
@@ -28,5 +28,13 @@ public interface FriendPersistenceRepository extends JpaRepository<FriendPersist
             """)
     boolean existsRelationship(@Param("userId") Long userId, @Param("friendId") Long friendId);
 
+    @Query("""
+            SELECT f FROM FriendPersistenceEntity f
+            WHERE (f.userId = :userId AND f.friendId = :friendId)
+               OR (f.userId = :friendId AND f.friendId = :userId)
+            ORDER BY f.id DESC
+            """)
+    List<FriendPersistenceEntity> findRelationship(@Param("userId") Long userId, @Param("friendId") Long friendId);
+
     void deleteByUserIdOrFriendId(Long userId, Long friendId);
 }
